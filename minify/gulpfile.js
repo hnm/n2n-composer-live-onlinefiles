1347572246a69b4b4660fd64e1b8703f64fc8662
Andreas von Burg <a@von-burg.net>
2018-09-22 02:44:23 +0200

    Update gulpfile.js

--- minify/gulpfile.js
+++ minify/gulpfile.js
@@ -12,5 +12,4 @@ gulp.task('minify-css', function (){
 	return gulp.src('../target/public/assets/**/*.css').pipe(cleanCSS()).pipe(gulp.dest('../target/public/assets'));
 });
 
-gulp.task('minify', ['uglify-js', 'minify-css'], function() {
-});
+gulp.task('minify', gulp.parallel('uglify-js', 'minify-css'));
