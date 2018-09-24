363021f489108a26ea9a30215be78e981c8fc66d
Andreas von Burg <a@von-burg.net>
2018-09-24 15:38:10 +0200

    minify updates

--- minify/gulpfile.js
+++ minify/gulpfile.js
@@ -2,14 +2,29 @@ var gulp = require('gulp');
 var uglifyes = require('gulp-uglify-es').default;
 var uglyComposer = require('gulp-uglify/composer');
 var uglify = uglyComposer(uglifyes, console);
-var cleanCSS = require('gulp-clean-css');
+//var csso = require('gulp-csso');
+let cleanCSS = require('gulp-clean-css');
 
 gulp.task('uglify-js', function () {
 	return gulp.src('../target/public/assets/**/*.js').pipe(uglifyes()).pipe(gulp.dest('../target/public/assets'));
 });
 
-gulp.task('minify-css', function (){
-	return gulp.src('../target/public/assets/**/*.css').pipe(cleanCSS()).pipe(gulp.dest('../target/public/assets'));
+//gulp.task('minify-css', function (){
+//	return gulp.src('../target/public/assets/**/*.css').pipe(csso({
+//        restructure: false,
+//        sourceMap: false,
+//        debug: true
+//    })).pipe(gulp.dest('../target/public/assets'));
+//});
+
+
+gulp.task('minify-css', () => {
+  return gulp.src('../target/public/assets/**/*.css')
+    .pipe(cleanCSS({debug: true}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+  .pipe(gulp.dest('../target/public/assets'));
 });
 
-gulp.task('minify', gulp.parallel('uglify-js', 'minify-css'));
+gulp.task('minify', gulp.series('uglify-js', 'minify-css'));
